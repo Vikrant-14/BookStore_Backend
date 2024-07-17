7c0d0bf13d0a70791da9f385e2fd601e32ed03e9
Vikrant-14 <vikrantgawale123@gmail.com>
2024-07-17 10:32:25 +0530

    [Vikrant] Refactor: refactored encryption class

--- RepositoryLayer/Utility/EncryptionHelper.cs
+++ RepositoryLayer/Utility/EncryptionHelper.cs
@@ -1,22 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RepositoryLayer.Utility
 {
     public static class EncryptionHelper
     {
-        private static readonly string EncryptionKey = GenerateRandomKey(256);
+        // Replace these with your actual Base64-encoded encryption key and IV
+        private static readonly string EncryptionKey = "MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE="; // Example: "MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE="
+        private static readonly string FixedIV = "MDEyMzQ1Njc4OTAxMjM0NQ=="; // Example: "MDEyMzQ1Njc4OTAxMjM0NQ=="
 
         public static string Encrypt(string plainText)
         {
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = Convert.FromBase64String(EncryptionKey);
-                aesAlg.IV = GenerateRandomIV(); // Generate a random IV for each encryption
+                aesAlg.IV = Convert.FromBase64String(FixedIV); // Use the fixed IV
 
                 aesAlg.Padding = PaddingMode.PKCS7; // Set the padding mode to PKCS7
 
@@ -31,7 +32,7 @@ namespace RepositoryLayer.Utility
                             swEncrypt.Write(plainText);
                         }
                     }
-                    return Convert.ToBase64String(aesAlg.IV.Concat(msEncrypt.ToArray()).ToArray());
+                    return Convert.ToBase64String(msEncrypt.ToArray());
                 }
             }
         }
@@ -43,13 +44,13 @@ namespace RepositoryLayer.Utility
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = Convert.FromBase64String(EncryptionKey);
-                aesAlg.IV = cipherBytes.Take(16).ToArray();
+                aesAlg.IV = Convert.FromBase64String(FixedIV); // Use the fixed IV
 
                 aesAlg.Padding = PaddingMode.PKCS7; // Set the padding mode to PKCS7
 
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                using (MemoryStream msDecrypt = new MemoryStream(cipherBytes, 16, cipherBytes.Length - 16))
+                using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
                 {
                     using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
@@ -60,33 +61,6 @@ namespace RepositoryLayer.Utility
                     }
                 }
             }
-        }
-
-        private static byte[] GenerateRandomIV()
-        {
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.GenerateIV();
-                return aesAlg.IV;
-            }
-        }
-
-        private static string GenerateRandomKey(int keySizeInBits)
-        {
-            // Convert the key size to bytes
-            int keySizeInBytes = keySizeInBits / 8;
-
-            // Create a byte array to hold the random key
-            byte[] keyBytes = new byte[keySizeInBytes];
-
-            // Use a cryptographic random number generator to fill the byte array
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                rng.GetBytes(keyBytes);
-            }
-
-            // Convert the byte array to a base64-encoded string for storage
-            return Convert.ToBase64String(keyBytes);
         }
     }
 }
